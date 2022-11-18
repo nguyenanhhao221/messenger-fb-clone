4e4cdd6df41127b2a9cd0ed49d97c54373339ab9
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-18 20:38:58 +0700

    docs: add comment

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -14,7 +14,6 @@ export const MessageList = () => {
     error,
     mutate,
   } = useSWR<{ result: TMessage[] }>('/api/getMessages', fetchMessages);
-  console.log('🚀 ~ MessageList ~ messagesData', messagesData);
 
   //  This will be used as a placeholder so that every time new message comes in, we will scroll to this div to make sure that we can see th latest message, otherwise user have to manually scroll down
   useEffect(() => {
@@ -26,16 +25,20 @@ export const MessageList = () => {
   // Before your web app can receive the event you publish, your web app needs to subscribe to the my-channel channel channel. Do this with pusher.subscribe.
   const channelName = 'message'; //update in case of changes
   const eventName = 'new-message';
-  const channel = clientPusher.subscribe(channelName);
 
   /**
    * When the component mount, establish a connection to the channel name and listen to the eventName.
    * Let's say we have 2 browser looking at the same chat, with the same account, the behavior we want is browser A send a message, browser B will see that same message
    * Keep in mind that this is the same account, we haven't implement message to other account yet in this stage
-   * When a message is sent, for example in Client A, that message will be optimistic update through the ChatInput component of Client A, and also our server will tell Pusher , Pusher will tell all client that currently subscribe to the channel (in this case client B) there is something happen
-   * But first
-   * If yes, this mean that our
+   * When a message is sent, for example in Client A, that message will be optimistic update through the ChatInput component of Client A, and also our server will tell Pusher , Pusher will tell all clients that currently subscribe to the channel (in this case client B & client A) "hey something happen"
+   * We can use the .bind method to bind client to the eventName, this method accept the eventName to listen to and a function that will be called every-time some thing happen to that channel, this function also carry a parameter which is the "message" that was created.
+   *
+   * So the idea is we can use this function to update all client with the new message return from the channel.
+   * But there is a catch, because Pusher cannot tell which one of the client is sending the message
+   * For example, if we run this function on all client, the Client A which originally send the message, Pusher will also tell client A to run that function and will cause duplicate message again
+   * So we need to check if the message id that was just created is already in the cache (because ChatInput of Client A will be optimistically updated with this new message), if this created message by Pusher is already exist in the cache, we don't return anything. If it is not, we run the function with the new message data and update the cache (in this case in client B)
    */
+  const channel = clientPusher.subscribe(channelName);
   useEffect(() => {
     channel.bind(eventName, async (newMessage: TMessage) => {
       const isSubscribeMessageExist = messagesData?.result.find(
