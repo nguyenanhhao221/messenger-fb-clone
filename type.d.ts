a59a5f7f178844612a383ae6167847fbedd536b6
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-12-09 20:34:08 +0700

    chores: types adjust

--- type.d.ts
+++ type.d.ts
@@ -1,12 +1,10 @@
 import type { z } from 'zod';
 import type { TypeMessage } from './components/ChatInput';
+import type { TypeRoom } from './utils/chatRoom';
+import type { TypeUser } from './utils/user';
 
 export type TMessage = z.infer<typeof TypeMessage>;
 
-export type TUser = {
-  id: string;
-  name: string;
-  image: string;
-};
+export type TRoom = z.infer<typeof TypeRoom>;
 
-export const formatChatRoomMessage = (arrOfMessageData: []) => {};
+export type TUser = z.infer<typeof TypeUser>;
